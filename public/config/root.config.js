4762f7110e5abe260cd2bd6cb375257ab808c5bb
therealdrtroll <akshaykadam7991@gmail.com>
2022-04-15 12:13:22 +0530

    initial commit

---
+++ public/config/root.config.js
@@ -0,0 +1,165 @@
+const config = {
+  personal: {
+    first_name: "Sharan",
+    last_name: "D",
+    age: "24",
+    about:
+      "Observent and innovatice full stack developer having specialized in MERN stack. Collaborative team player. I have a strong motive to make myself a valued asset and passionate about learning new skills",
+  },
+  education: [
+    {
+      specialization: "Full Stack Web Development",
+      duration: "Oct 2021 - May 2022",
+      university: "Masai School",
+      details: "Specialized MERN Stack",
+    },
+    {
+      specialization: "B.E Civil Engineering",
+      duration: "Jun 2015 - Jul 2019",
+      university: "Vishveshwarya University",
+      details: "Structues, Construction Management and Hydrology",
+    },
+  ],
+  skills: {
+    tech: [
+      {
+        title: "HTML",
+        id: "html",
+      },
+      {
+        title: "CSS",
+        id: "css",
+      },
+      {
+        title: "JavaScript",
+        id: "javascript",
+      },
+      {
+        title: "MongoDB",
+        id: "mongodb",
+      },
+      {
+        title: "Express",
+        id: "express",
+      },
+      {
+        title: "Node",
+        id: "node",
+      },
+      {
+        title: "REST Api",
+        id: "rest",
+      },
+      {
+        title: "React",
+        id: "react",
+      },
+      {
+        title: "Redux",
+        id: "redux",
+      },
+      {
+        title: "DSA",
+        id: "dsa",
+      },
+    ],
+    soft: [
+      {
+        title: "Team Work",
+        id: "team_work",
+      },
+      {
+        title: "Effective Communication",
+        id: "effective_communication",
+      },
+      {
+        title: "Problem Solving",
+        id: "problem_solving",
+      },
+      {
+        title: "Time Management",
+        id: "time_management",
+      },
+      {
+        title: "Remote Work Collboration",
+        id: "remote_collaboration",
+      },
+    ],
+  },
+  experience: [
+    {
+      role: "Consulting Engineer and Contractor",
+      company: "Aashraya properties",
+      duration: "Jul 2020 - Oct 2021",
+      details: [
+        "Experienced with planning, design and execution.",
+        "Worked on 4 residential buildings.",
+      ],
+    },
+    {
+      role: "Quantity Surveyor - Intern",
+      company: "Apaha Trainers and consultants Pvt.Ltd, Pune",
+      duration: "Jun 2020 - Jul 2021",
+      details: [
+        "Calcalculated Quantity of Materials required at various stages of work on a G+10 residential appartment, Pune",
+      ],
+    },
+  ],
+  projects: [
+    {
+      title: "Nordstorm Clone",
+      description:
+        "A US based Fashion E-Commerce Website A Fashion E-Commerce Website",
+      features: ["Authentication", "Buy product, Add to cart, Payment"],
+      github: "https://github.com/sharanweb/nordstorm",
+      blog: "",
+      teamSize: 4,
+      techStack: [
+        {
+          title: "HTML",
+          id: "html",
+        },
+        {
+          title: "CSS",
+          id: "css",
+        },
+        {
+          title: "JavaScript",
+          id: "javascript",
+        },
+      ],
+    },
+    {
+        title: "LifeStyle clone",
+        description:
+          "A Fashion E-Commerce Website",
+        features: ["Authentication", "Buy product, Add to cart, Payment"],
+        github:"https://github.com/sharanweb/LIFESTYLE",
+        blog:"",
+        teamSize: 5,
+        techStack: [
+          {
+            title: "HTML",
+            id: "html",
+          },
+          {
+            title: "CSS",
+            id: "css",
+          },
+          {
+            title: "JavaScript",
+            id: "javascript",
+          },
+        ],
+      }
+  ],
+  contact: {
+      email: "sharan.d1997@gmail.com",
+      phone: "8050926778",
+      linkedin: "https://www.linkedin.com/in/sharandooganavar/",
+      medium: "https://medium.com/@sharan.d1997",
+      twitter: ""
+  },
+};
+
+window.root = config;
